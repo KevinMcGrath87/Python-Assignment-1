b5c138cf2e2f89a1ce6d9816543a2402c2eabf68
Kevin McGrath <kpmcgra@gmail.com>
2022-08-29 16:19:44 -0500

    first commit of this assignment

---
+++ hello_world_2.py
@@ -0,0 +1,18 @@
+#hello world assignment
+print("Hello World!")
+name = "Noelle"
+helloWorld = "Hello World"
+print("{}{}".format(helloWorld.replace("World", ""), name))
+# f"{helloWorld.replace("World","")}"
+f"{helloWorld}"
+f"{helloWorld.lower()}"
+print(f"{helloWorld.replace('World','')}")
+
+print("Hello "+name)
+name = 42
+print("hello",str(name))
+print("Hello " + str(name))
+fave1="sushi"
+fave2="pizza"
+print("I love to eat {} and {}".format(fave1,fave2))
+print(f"I love to eat {fave1} and {fave2}")
